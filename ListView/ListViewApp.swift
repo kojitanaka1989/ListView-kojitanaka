48938861bfc18a653f7308f47b0dafc8049e474d
Koji <yourpartner2021@gmail.com>
2025-02-09 14:34:30 +0900

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by 田中康志 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
